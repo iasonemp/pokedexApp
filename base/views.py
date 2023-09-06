06b310f2b660b3f1d92e3479093f2076ee4e94c3
iasonemp <iasonemp@gmail.com>
2023-09-06 06:19:48 +0300

    added pokemon name and type homepage

--- base/views.py
+++ base/views.py
@@ -5,8 +5,16 @@ import requests
 # Create your views here.
 
 def home(request):
-    response=requests.get('https://pokeapi.co/api/v2/pokemon/').json()
-    context = {'response': response}
+    response=requests.get('https://pokeapi.co/api/v2/pokemon/1').json()
+    pokemonDict = {}
+    pokemonDict['id'] = response['id']
+    pokemonDict['name'] = response['name']
+    pokemonTypes = []
+    for item in response['types']:
+        typeName = item['type']['name']
+        pokemonTypes.append(typeName)
+    pokemonDict['types'] = pokemonTypes
+    context = {'pokemonName': pokemonDict['name'], 'pokemonTypes': pokemonDict['types']}
     return render(request, 'components/home.html', context)
 
 def login(request):
