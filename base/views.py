ccded09fc5e72e53647a8abc4dc122a4f3aaf849
iasonemp <iasonemp@gmail.com>
2023-09-08 22:19:58 +0300

    looped through API

--- base/views.py
+++ base/views.py
@@ -5,18 +5,21 @@ import requests
 # Create your views here.
 
 def home(request):
-    response=requests.get('https://pokeapi.co/api/v2/pokemon/1').json()
-    pokemonTypes = []
-    for item in response['types']:
-        typeName = item['type']['name']
-        pokemonTypes.append(typeName)
-    pokemonDict = {
-    'id': response['id'],
-    'sprite': response['sprites']['other']['dream_world']['front_default'],
-    'name': response['name'],
-    'types': pokemonTypes,
-    }
-    context = {'pokemonDict': pokemonDict}
+    pokemonList = []
+    for i in range(1, 3):
+        response=requests.get(f'https://pokeapi.co/api/v2/pokemon/{i}').json()
+        pokemonTypes = []
+        for item in response['types']:
+            typeName = item['type']['name']
+            pokemonTypes.append(typeName)
+        pokemonDict = {
+        'id': response['id'],
+        'sprite': response['sprites']['other']['dream_world']['front_default'],
+        'name': response['name'],
+        'types': pokemonTypes,
+        }
+        pokemonList.append(pokemonDict)
+    context = {'pokemonList': pokemonList}
     return render(request, 'components/home.html', context)
 
 def login(request):
