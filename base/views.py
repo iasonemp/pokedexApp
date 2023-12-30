80721dbf4a2627d0208f6fa59985a24e173a7864
iasonemp <iasonemp@gmail.com>
2023-12-30 22:23:19 +0200

    fixed search bug for unauthenticated user

--- base/views.py
+++ base/views.py
@@ -298,27 +298,34 @@ def home(request):
     name_query = None
     type_query = None
     
-    # Retrieve all pokemons
     if request.user.is_authenticated:
-        # if search_query:  # Get the search query from the request parameters
-        #     user_favorites = request.user.favorites.filter(name__icontains=search_query)
-        #     other_pokemons = Pokemon.objects.filter(name__icontains=search_query).exclude(name__in=user_favorites.values('name'))
+        # Authenticated, name search
         if 'name' in search_query.keys() and 'type' not in search_query.keys():
-            name_search_query = search_query.get('name')
-            user_favorites = request.user.favorites.filter(name__icontains=name_search_query)
-            other_pokemons = Pokemon.objects.filter(name__icontains=name_search_query).exclude(name__in=user_favorites.values('name'))
             name_query = search_query.get('name')
+            user_favorites = request.user.favorites.filter(name__icontains=name_query)
+            other_pokemons = Pokemon.objects.filter(name__icontains=name_query).exclude(name__in=user_favorites.values('name'))
+        # Authenticated, type search
         elif 'type' in search_query.keys() and 'name' not in search_query.keys():
-            type_search_query = search_query.get('type')
-            user_favorites = request.user.favorites.filter(types__icontains=type_search_query)
-            other_pokemons = Pokemon.objects.filter(types__icontains=type_search_query).exclude(name__in=user_favorites.values('name'))
             type_query = search_query.get('type')
+            user_favorites = request.user.favorites.filter(types__icontains=type_query)
+            other_pokemons = Pokemon.objects.filter(types__icontains=type_query).exclude(name__in=user_favorites.values('name'))
+        # Authenticated, all pokemons
         else:
                 user_favorites = request.user.favorites.all()
                 other_pokemons = Pokemon.objects.exclude(name__in=user_favorites.values('name'))
         all_pokemons = list(user_favorites) + list(other_pokemons)
     else: 
-        all_pokemons = Pokemon.objects.all()
+        # Unauthenticated, name search
+        if 'name' in search_query.keys() and 'type' not in search_query.keys():
+            name_query = search_query.get('name')
+            all_pokemons = Pokemon.objects.filter(types__icontains=name_query)
+        # Unauthenticated, type search
+        elif 'type' in search_query.keys() and 'name' not in search_query.keys():
+            type_query = search_query.get('type')
+            all_pokemons = Pokemon.objects.filter(types__icontains=type_query)
+        # Unauthenticated, all pokemons search
+        else:
+            all_pokemons = Pokemon.objects.all()
     
     pokemon_list = []
     for pokemon in all_pokemons:
