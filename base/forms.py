dfa3719753d8d9a7ea062bd1c2e6cff6c5f7370f
iasonemp <iasonemp@gmail.com>
2024-01-06 16:43:16 +0200

    turned off autocomplete for forms

--- base/forms.py
+++ base/forms.py
@@ -8,7 +8,10 @@ class MyUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
-
+        widgets = {
+                    'username': forms.TextInput(attrs={'autocomplete': 'off'}),
+                    'email': forms.EmailInput(attrs={'autocomplete': 'off'}),
+                }
 class CommentForm(ModelForm):
     class Meta:
         model = Comment
